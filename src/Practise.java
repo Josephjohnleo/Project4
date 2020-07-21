49965836e3bf920c42e9cf01a41f5e29bb208ed8
Josephjohnleo <josephjohnleoindia@gmail.com>
2020-07-21 02:23:02 -0700

    John's Code

--- src/Practise.java
+++ src/Practise.java
@@ -1,6 +1,11 @@
 
 public class Practise {
+	public void Hello() {
+		System.out.println("Hai");
+	}
 public static void main(String[] args) {
 	System.out.println("Ave Maria");
+	Practise p = new Practise();
+	p.Hello();
 }
 }
