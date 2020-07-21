8d0353f2f1c249db09dea09055b83f2c03a54b3f
Josephjohnleo <josephjohnleoindia@gmail.com>
2020-07-21 02:37:31 -0700

    commited the code

--- src/Practise.java
+++ src/Practise.java
@@ -3,6 +3,9 @@ public class Practise {
 	public void Hello() {
 		System.out.println("Hai");
 	}
+	public void hei() {
+		System.out.println("helloee");
+	}
 public static void main(String[] args) {
 	System.out.println("Ave Maria");
 	Practise p = new Practise();
