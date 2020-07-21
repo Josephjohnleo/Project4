bcb1b3782f33194fc8dbb9eae4d9fef45123624c
Josephjohnleo <josephjohnleoindia@gmail.com>
2020-07-21 00:43:06 -0700

    conflict check

--- src/Practise.java
+++ src/Practise.java
@@ -1,6 +1,13 @@
 
 public class Practise {
+	
+	public void page() {
+		System.out.println("this code is for Payment Page");
+	}
+	
 public static void main(String[] args) {
-	System.out.println("Ave Maria");
+	System.out.println("Content");
+	Practise p =new Practise();
+	p.page();
 }
 }
